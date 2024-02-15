3ec3577f91dae2fb41ec8d07d2a0bc397f0f57f9
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2024-02-15 18:22:51 +0600

    added vercel.json and modified cors policy in index.js and deployment completed

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware 
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json());
 
 
