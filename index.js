b0a2a60b4c322d53d6eafa02c8cc81ca3e0a8da7
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2024-02-15 16:23:33 +0600

    delete a specifil user from database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -39,6 +39,13 @@ async function run() {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    })
+
+    app.delete(`/user/:id`,async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
     })
 
 
